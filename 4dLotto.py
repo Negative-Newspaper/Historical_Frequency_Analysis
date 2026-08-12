1422307acf2beece37a2e4314256a2666f50bb9e
Howard <83273907+Negative-Newspaper@users.noreply.github.com>
2026-08-13 00:06:57 +0800

    add some functions

--- 4dLotto.py
+++ 4dLotto.py
@@ -1,55 +1,61 @@
 import csv
 import matplotlib.pyplot as plt
 
-winning_numbers = []
 
 
-with open("4d_lotto.csv", mode= "r")as file:
-    reader = csv.DictReader(file)
-        
-    for row in reader:
-        
-        validation = True
+def load_and_validate_data(csv_data):
 
-        if not row["COMBINATIONS"]:
-            validation = False
-        else: 
-            if "" in row["COMBINATIONS"].split("-"):
+    winning_numbers = []
+
+
+    with open(csv_data, mode= "r")as file:
+        reader = csv.DictReader(file)
+            
+        for row in reader:
+            
+            validation = True
+
+            if not row["COMBINATIONS"]:
                 validation = False
-                break
-                      
-            for part in row["COMBINATIONS"].split("-"):
-                if part.isalpha():
-                    validation = False
-                    break
-                elif part.isdigit() and int(part) > 9:
-                    validation = False
-                    break
-                elif len(row["COMBINATIONS"].split("-")) > 4 or len(row["COMBINATIONS"].split("-")) < 4:
-                    validation = False
-                    break
+            else: 
+                if "" in row["COMBINATIONS"].split("-"):
+                   validation = False
                     
-        if validation == True:
-            winning_numbers.append(row)
- 
+                        
+                for part in row["COMBINATIONS"].split("-"):
+                    if part.isalpha():
+                        validation = False
+                        break
+                    elif part.isdigit() and int(part) > 9:
+                        validation = False
+                        break
+                    elif len(row["COMBINATIONS"].split("-")) > 4 or len(row["COMBINATIONS"].split("-")) < 4:
+                        validation = False
+                        break
+                        
+            if validation == True:
+                winning_numbers.append(row)
+    return winning_numbers
 
 
 #### counting repeating numbers ####
-number_of_winning_numbers = {}
 
-for row in winning_numbers:
-    winning_number = row["COMBINATIONS"]
-    if winning_number in number_of_winning_numbers:
-        number_of_winning_numbers[winning_number] += 1
-    else:
-        number_of_winning_numbers[winning_number] = 1
-            
-for number in number_of_winning_numbers:
-    repeated = number_of_winning_numbers[number]
-    
-    if repeated > 2:
-        print(f"the number {number} repeated {repeated}")
+def counting_repeating_numbers(load_and_validate_data):
+    number_of_winning_numbers = {}
+
+    for row in load_and_validate_data:
+        winning_number = row["COMBINATIONS"]
+        if winning_number in number_of_winning_numbers:
+            number_of_winning_numbers[winning_number] += 1
+        else:
+            number_of_winning_numbers[winning_number] = 1
+                
+    for number in number_of_winning_numbers:
+        repeated = number_of_winning_numbers[number]
         
+        if repeated > 2:
+            print(f"the number {number} repeated {repeated}")
+            
 #### counting repeating numbers by positions ###
 
 def count_position(list_of_winning_numbers, position):
@@ -68,30 +74,34 @@ def count_position(list_of_winning_numbers, position):
     #### sort the keys ####        
     keys = (['0','1','2','3','4','5','6','7','8','9'])
     for key in keys:
-        if not position_winning_number[key] == "":
+        if key in position_winning_number:
             sorted_dictionary[key] = position_winning_number[key]
-
+        
     return sorted_dictionary
 
 #### Printing chart ####
 
-def show_chart(position):
-    data = count_position(winning_numbers, position)
+def show_chart(load_and_validate_data, position):
+    data = count_position(load_and_validate_data, position)
 
     x_labels = (data.keys())
     y_values = (data.values())
 
     plt.figure(figsize=(8,6))
     plt.bar(x_labels, y_values, color='skyblue', edgecolor='black')
+    plt.title(f"Position {position} Frequesncy Chart")
     plt.xlabel('Digits')
     plt.ylabel('Frequency')
 
     plt.show()
     
-show_chart(1)
-show_chart(2)
-show_chart(3)
-show_chart(4)
+
+validated_csv = load_and_validate_data("4D_lotto.csv")
+
+show_chart(validated_csv, 1)
+show_chart(validated_csv, 2)
+show_chart(validated_csv, 3)
+show_chart(validated_csv, 4)
 
 # sample di pa ako marunong mag cowork d2
 
